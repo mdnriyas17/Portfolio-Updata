062828140706f7c7ad603a188172ccf9549c3d16
mdnriyas17 <mdnriyas@gmail.com>
2023-09-13 09:03:34 +0530

    Project Add

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -6,115 +6,97 @@ import "./Project.css";
 function Project() {
   const [value, setValue] = useState("all");
   let data = [
-    // {
-    //   key: ["react", "express", "all", "restApi", "mern", "node"],
-    //   img: "./asset/project/wetherapp.png",
-    //   title: "Grocery Store",
-    //   githubFrontEnd:
-    //     "https://github.com/Shersha123/glocery-frontend.git",
-    //   githubBackEnd:
-    //     "https://github.com/Shersha123/glocery-backend.git",
-    //   sourceCode: "https://glocery-frontend.vercel.app",
-    //   shortNote:
-    //     "I built a ecommerce web application for grocery store.",
-    //   longNote: [
-    //     "The frontend is built using **React** with **Bootstrap** for styling. The backend is build with **Node.js** , **Express.js** and used **MongoDB** as database.",
-    //     "They are **two category of rolls admin and users**. If a **user login as admin** it will show the **admin page** or else it will show **User page**.",
-    //     "Admin page to show **add edit delete a product** and show **available stocks**, **bought stock**, **selling stock**, **billing (or) order data by user wise**.",
-    //     "User page to show **product** **add to cart**,**Razorpay method**, **your order page**, **profile page** and finaly **invoice bill send in your email** etc.",
-    //   ],
-    //   skills: [
-    //     {
-    //       name: "React",
-    //   link: "https://img.icons8.com/officel/40/react.png",
+    {
+      key: ["react", "express", "all", "restApi", "mern", "node"],
+      img: "./asset/project/bookmyshow.png",
+      title: "BookMyShow",
+      githubFrontEnd:
+        "https://github.com/mdnriyas17/booking-f/tree/main",
+      githubBackEnd:
+        "https://github.com/mdnriyas17/booking-b",
+      sourceCode: "https://comforting-basbousa-b02e94.netlify.app/",
+      shortNote:
+        "I built a Bookmyshow web application for Movie Ticket Booking.",
+      longNote: [
+        "The frontend is built using **React** with **Bootstrap** for styling. The backend is build with **Node.js** , **Express.js** and used **MongoDB** as database.",
+        "They are **two category of rolls admin and users**. If a **user login as admin** it will show the **admin page** or else it will show **User page**.",
+        "Admin page to show **add edit delete a Movie**.",
+        "User page to show **Ticket** **add to cart**,**Razorpay method**, **your order page**, **profile page** and finaly **invoice bill send in your email** etc.",
+      ],
+      skills: [
+        {
+          name: "React",
+          link: "https://img.icons8.com/officel/40/react.png",
 
-    //     },
-    //     {
-    //       name: "redux-toolkit",
-    //       link: "./asset/redux.svg",
-    //     },
-    //     {
-    //       name: "Node",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-    //     },
-    //     {
-    //       name: "Express",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
-    //     },
-    //     {
-    //       name: "Mongo DB",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
-    //     },
+        },
 
-    //     {
-    //       name: "Bootstrap",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
-    //     },
-    //     {
-    //       name: "Chart JS",
-    //       link: "./asset/chartjs.svg",
-    //     },
-    //     {
-    //       name: "JWT",
-    //       link: "./asset/jwt.svg",
-    //     },
-    //     {
-    //       name: "Razorpay",
-    //       link: "./asset/razorpay.svg",
-    //     },
-    //   ],
-    // },
-    // {
-    //   key: ["react", "express", "all", "restApi", "mern","node"],
-    //   img: "./asset/project/zendesk.png",
-    //   title: "Inventory Management",
-    //   githubFrontEnd:
-    //     "https://github.com/Sivav15/shop-inventory-management-front-end",
-    //   githubBackEnd:
-    //     "https://github.com/Sivav15/Shop-inventory-management-back-end",
-    //   sourceCode:"https://shopinventorymanagement.netlify.app",
-    //   shortNote:
-    //     "I built an app for shop inventory management for billing.",
-    //   longNote: [
-    //     "The frontend is built using **React** with **Bootstrap** for styling. The backend is build with **Node.js** , **Express.js** and used **MongoDB** as database.",
-    //     "This app used for **retail shop inventory management**, To **store product** and **stock** details.",
-    //     "I have add **login/logout** feature. If the **users** is **logged in**  it will dissplay the users **data**. ",
-    //     "They are **two category of users-admin and billing speciallest**. If a **user login as admin** it will show the **admin page** or else it will show **billing session**.",
-    //     "Admin page to show **add edit delete product** and show **available stocks**, **bought stock**, **selling stock**, **billing (or) order data by user wise**, **yearly selling amount**.",
-    //     "User page to show **billing section**, **your order page** and **invoice bill** etc.",
-    //   ],
-    //   skills: [
-    //     {
-    //       name: "React",
-    //       link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
-    //     },
-    //     {
-    //       name: "Node",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-    //     },
-    //     {
-    //       name: "Express",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
-    //     },
-    //     {
-    //       name: "Mongo DB",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
-    //     },
+        {
+          name: "Node",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        },
+        {
+          name: "Express",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        },
+        {
+          name: "Mongo DB",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
+        },
 
-    //     {
-    //       name: "Bootstrap",
-    //       link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
-    //     },
-    //     {
-    //       name: "Chart JS",
-    //       link: "./asset/chartjs.svg",
-    //     },
-    //     {
-    //       name: "Razorpay",
-    //       link: "./asset/razorpay.svg",
-    //     },
-    //   ],
-    // },
+        {
+          name: "Bootstrap",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        },
+
+
+
+      ],
+    },
+    {
+      key: ["react", "express", "all", "restApi", "mern", "node", "JWT"],
+      img: "./asset/project/cms.png",
+      title: "Content Management System",
+      githubFrontEnd:
+        "https://github.com/mdnriyas17/cms-client",
+      githubBackEnd:
+        "https://github.com/mdnriyas17/cms-server",
+      sourceCode: "https://main--startling-seahorse-338dd2.netlify.app",
+      shortNote:
+        "I built an app for shop inventory management for billing.",
+      longNote: [
+        "The frontend is built using **React** with **Bootstrap** for styling. The backend is build with **Node.js**. **JWT** for login, **Express.js** and used **MongoDB** as database.",
+        "This app used for **Content management**, To **Show the Content** and **Content** details.",
+        "I have add **login/logout** feature. If the **users** is **logged in**  it will dissplay the users **data**. ",
+
+      ],
+      skills: [
+        {
+          name: "React",
+          link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+        },
+        {
+          name: "Node",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        },
+        {
+          name: "Express",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        },
+        {
+          name: "Mongo DB",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
+        },
+
+        {
+          name: "Bootstrap",
+          link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        },
+        {
+          name: "JWT",
+          link: "./asset/jwt.svg",
+        },
+      ],
+    },
     {
       key: ["javascript", "all", "mockApi"],
       img: "./asset/project/weather-icons-collection_1167-124.png",
@@ -323,23 +305,7 @@ function Project() {
         <h1>
           My Recent <span className="project-highlight">Works</span>
         </h1>
-        <div className="dropdown">
-          <select
-            name=""
-            id=""
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          >
-            <option value="all">All</option>
-            <option value="javascript">JavaScript DOM</option>
-            <option value="react">React JS</option>
-            <option value="node">Node JS</option>
-            <option value="mockApi">Mock Api</option>
-            <option value="restApi">Rest Api</option>
-            <option value="mern">MERN</option>
 
-          </select>
-        </div>
         <div className="row project-container">
           {datas.length > 0 &&
             datas.map((item, index) => {
